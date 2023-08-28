45aa27dfe7f0a6d5bda69792587e57dc6d1a246d
ParkChaeWon <aeoo@naonsoft.com>
2023-08-28 15:48:37 +0900

    Section6. Users Microservice - AuthenticationFilter 추가
    - 20230827

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -1,8 +1,11 @@
 package com.example.userservice.security;
 
+import jakarta.servlet.Filter;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +21,20 @@ public class WebSecurity{
         MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
         return http.csrf(csrf -> csrf.disable())
                 .headers(authorize -> authorize.frameOptions(frameOptions -> frameOptions.disable()))
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers(mvcMatcherBuilder.pattern("/**")).permitAll())
+                .authorizeRequests(authorize -> {
+                    try {
+                        authorize.requestMatchers(mvcMatcherBuilder.pattern("/**"))
+                                .hasIpAddress("192.168.0.8").and()
+                                .addFilter(getAuthenticationFilter(http));
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                })
                 .getOrBuild();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter(HttpSecurity http) throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(http.getSharedObject(AuthenticationConfiguration.class).getAuthenticationManager());
+        return authenticationFilter;
     }
 }
